e62173ce2f1c82285de16c701fd1cc156e220f8a
Dmitry Shin <dmitry.s@hsdn.org>
2021-04-03 21:14:09 +0400

    Update

--- index.js
+++ index.js
@@ -7,12 +7,11 @@ module.exports = function GInvite(mod) {
 
 	mod.command.add(["ginv", "invg"], () => {
 		enabled = !enabled;
+
 		mod.command.message(`Module ${ enabled ? "enabled" : "disabled"}`);
 	});
 
 	mod.hook("S_SPAWN_USER", 17, event => {
-		if (enabled) console.log(event.name, event.gameId);
-
 		if (!event.guildName && enabled) {
 			const delay = Math.random() * (MAX_DELAY - MIN_DELAY) + MIN_DELAY;
 
